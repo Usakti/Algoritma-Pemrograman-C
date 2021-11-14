b6062f032d5a8831b8bdcb6ba565d9988f359f17
Azhar Rizki Zulma <71958079+AzharRizky@users.noreply.github.com>
2021-11-14 12:34:52 +0700

    Create Modul 2 - Latihan 2.cpp

---
+++ Praktikum 2/Modul 2 - Latihan 2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(void) {
+	int c;
+	int f;
+	cout<<"-----------------------------------------------"<<endl;
+	cout<<"| Program Konversi Suhu Fahrenheit ke Celcius |"<<endl;
+	cout<<"| Nama: Azhar Rizky Zulma   NIM: 065001900001 |"<<endl;
+	cout<<"-----------------------------------------------"<<endl;
+	cout<<"Masukkan Nilai Derajat Fahrenheit:"<<ends; cin>>f;
+	cout<<endl;
+	c = (f-32)*5/9;
+	cout<<"Hasil dari "<<f<<" derajat Fahrenheit adalah "<<c<<" derajat Celcius"<<endl;
+	cout<<"diketahui derajat Fahrenheit: "<<c<<endl;
+	cout<<"perhitungan derajat Fahrenheit ke Celcius"<<endl;
+	cout<<"Rumus:"<<endl;
+	cout<<"C = (f - 32) * 9/5"<<endl;
+	cout<<"C = ("<<f<<" - 32) * 9/5"<<endl;
+	cout<<"C = "<<f-32<<" * 9/5"<<endl;
+	cout<<"C = "<<(f-32)*5/9<<" Derajat"<<endl;
+}
