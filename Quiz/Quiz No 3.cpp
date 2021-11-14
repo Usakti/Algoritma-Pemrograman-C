0ab11757836bd4a331394e71aa5a0993189bd88b
Azhar Rizki Zulma <71958079+AzharRizky@users.noreply.github.com>
2021-11-14 12:57:54 +0700

    Create Quiz No 3.cpp

---
+++ Quiz/Quiz No 3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(void)
+{
+	int x, y, z;
+	float hasil;
+	cout<<"----------------------------------------------"<<endl;
+	cout<<"| Nama: Azhar Rizky Zulma  NIM: 065001900001 |"<<endl;
+	cout<<"----------------------------------------------"<<endl;
+	cout<<"Masukkan Jumlah = "<<ends; cin>>x;
+	
+	for (y=x; y>=1; y--) {
+        for (z=y; z>=1; z--) {
+            cout<<y;
+        }
+        cout<<endl;
+    }
+}
