68ce1cc279e589637d7e44af4536ba04a3da386f
Azhar Rizki Zulma <71958079+AzharRizky@users.noreply.github.com>
2021-11-14 12:57:10 +0700

    Create Quiz No 1.cpp

---
+++ Quiz/Quiz No 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(void)
+{
+	int x, y, a, b;
+	float hasil;
+	cout<<"----------------------------------------------"<<endl;
+	cout<<"| Nama: Azhar Rizky Zulma  NIM: 065001900001 |"<<endl;
+	cout<<"----------------------------------------------"<<endl;
+	cout<<"Masukkan Nilai x = "<<ends; cin>>x;
+	cout<<"Masukkan Nilai y = "<<ends; cin>>y;
+ 	cout<<"Masukkan Nilai a = "<<ends; cin>>a;
+ 	cout<<"Masukkan nilai b = "<<ends; cin>>b;
+ 	
+ 	hasil = sqrt(pow((2*x),3)-pow(a, 2)*b)/sqrt(5*y);
+ 	
+	cout<<"Hasil = "<<hasil<<endl;
+}
